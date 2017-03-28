7be17605ee4da4a2524b7c55d9b68592c7b12ccf
Eddie Waller <edwardw@zoosk.com>
2017-03-28 16:04:19 -0700

    Removing GPUImage old library with pods.

--- YUGPUImageHighPassSkinSmoothingDemo/Pods/Target Support Files/Pods-YUGPUImageHighPassSkinSmoothingDemo/Pods-YUGPUImageHighPassSkinSmoothingDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YUGPUImageHighPassSkinSmoothingDemo : NSObject
-@end
-@implementation PodsDummy_Pods_YUGPUImageHighPassSkinSmoothingDemo
-@end
